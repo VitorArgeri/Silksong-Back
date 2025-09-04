46091478fd23234f38d52419784b9ba30cfadb29
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-09-04 09:35:01 -0300

    feat: add index Router

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,10 +2,10 @@ import express from "express";
 
 // Importar todas as rotas
 import authRouter from "./auth.routes.js";
-import animesRouter from "./animeRoutes.js";
-import personagensRouter from "./personagemRoutes.js";
-import collectionRouter from "./collectionRoutes.js";
-import cardRouter from "./cardRoutes.js";
+import diariesRouter from "./diaryRouter.js";
+import npcRouter from "./npcRouter.js";
+import enemyRouter from "./enemyRouter.js";
+import bossRouter from "./bossRouter.js";
 
 import authMiddleware from "../middleware/authMiddleware.js";
 
@@ -13,13 +13,12 @@ const router = express.Router();
 
 // Rotas públicas
 router.use("/auth", authRouter);
-router.use("/collections", collectionRouter);
-router.use("/cards", cardRouter);
+router.use("/diaries", diariesRouter);
+router.use("/npcs", npcRouter);
+router.use("/enemies", enemyRouter);
+router.use("/bosses", bossRouter);
 
 // Rotas protegidas
 router.use(authMiddleware);
-
-router.use("/animes", animesRouter);
-router.use("/personagens", personagensRouter);
 
 export default router;
