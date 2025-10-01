6367f3e7e0ec78bce83ee94d2a3831362658c573
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-10-01 14:43:29 -0300

    delete: npc, boss and enemies

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,9 +3,6 @@ import express from "express";
 // Importar todas as rotas
 import authRouter from "./auth.routes.js";
 import diariesRouter from "./diaryRouter.js";
-import npcRouter from "./npcRouter.js";
-import enemyRouter from "./enemyRouter.js";
-import bossRouter from "./bossRouter.js";
 import characterRouter from "./characterRouter.js";
 
 import authMiddleware from "../middleware/authMiddleware.js";
@@ -15,9 +12,6 @@ const router = express.Router();
 // Rotas públicas
 router.use("/auth", authRouter);
 router.use("/diaries", diariesRouter);
-router.use("/npcs", npcRouter);
-router.use("/enemies", enemyRouter);
-router.use("/bosses", bossRouter);
 router.use("/characters", characterRouter);
 
 // Rotas protegidas
